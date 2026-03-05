8c45bde5e8bbe8b7009380e55ab5130c3f851318
Ethan <mingam2007@gmail.com>
2026-03-05 12:07:47 +0100

    début de l'ex philo

--- movies.js
+++ movies.js
@@ -1,7 +1,6 @@
 // OMDB API -> https://www.omdbapi.com/
 
 // Faire une barre de recherche avec bouton pour rechercher et ajouter en favoris 
-
 // On doit pouvoir recghercher un film par son nom et afficher les résultatrs en dessous 
 // On doit également pouvoir accéder à une liste de fazvoris en cliquant sur le bouton de favoris 
 // Chaque div de film doit contenir un bouton d'ajout au favoris / suppression des favoris sui déjà dedans 
@@ -9,32 +8,74 @@
 
 // BONUS : Tenter de le faire avec un système de classes et d'objets 
 
-// Récupération des éléments HTML
-const apiUrl = "http://www.omdbapi.com/?apikey=eed08b06&s="
-const input = document.querySelector(".searchInput")
-const searchBtn = document.querySelector(".searchBtn")
-const favBtn = document.querySelector(".favBtn")
-const resultsZone = document.querySelector(".resultsZone")
+// Cette classe est un peu la classe globale de l'application
+class MovieApp {
+    // C'est cette classe que l'on devra instancier lors de notre arrivée sur la page de movies  
+    // On recup les éléments HTML ici. On pourra aussi initialiser le tableau de favs dans cette classe (new FavManager)
+    // On gère également la recherche de films, l'affichage de la page des favoris (ex : showFav())
+    // Il faudra également les écouteurs d'événement 
+    constructor() {
+        this.apiUrl = "http://www.omdbapi.com/?apikey=eed08b06&s="
+        this.input = document.querySelector(".searchInput")
+        this.searchBtn = document.querySelector(".searchBtn")
+        this.favBtn = document.querySelector(".favBtn")
+        this.resultsZone = document.querySelector(".resultsZone")
 
-let favs = JSON.parse(localStorage.getItem("favorites")) || []
+        this.searchBtn.addEventListener("click", () => this.searchMovie(this.input.value)) 
+        this.favBtn.addEventListener("click", () => this.showFav())
 
-// 3 aspects : Movie, MovieApp, FavManager
 
-// Réfléchirf et coder les 2 prochaines classes : MovieApp et FavManager 
+    }
 
-// Dans la classe MovieApp vous aurez les fonctionnalités liées à la recherche de film, 
-// donc par exemple searchMovie() 
+    async searchMovie(query) {
+        this.resultsZone.innerHTML = ""
+        this.input.value = ""
+    
+        const res = await fetch(this.apiUrl + query, { headers : {
+            "Accept": "application/json"
+        }})
+    
+        const data = await res.json()
+    
+        if (!data.Response) {
+            this.resultsZone.innerHTML = "Aucun film trouvé ... " + data.Error
+            return
 
-// Dans la classe FavManager les fonctionnalités liées aux favoris -> Dans cette classe vous aurez 
-// la propriété favs, les méthodes addToFav() etc
+        } else {
+            let movies = data.Search 
+            console.log(movies)
+    
+            movies.forEach(movie => {
+                let card = new Movie(movie).render()
+                this.resultsZone.append(card)
+            })
+        }
+    }
 
+    showFav() {
+        this.resultsZone.innerHTML = ""
+
+        let favs = new FavManager().getFavs()
+
+        if (!favs.length) {
+            this.resultsZone.innerText = "Aucun favori enregistré"
+        } else {
+            favs.forEach(fav => {
+                let card = new Movie(fav).render()
+                this.resultsZone.append(card)
+            })
+        }
+    }
+}
+
+// La classe Movie permet de générer une card pour un film 
 class Movie {
     constructor(movie) {
-        this.id = movie.imdbId
-        this.title = movie.Title
-        this.poster = movie.Poster
-        this.type = movie.Type
-        this.year = movie.Year 
+        this.imdbId = movie.imdbId
+        this.Title = movie.Title
+        this.Poster = movie.Poster
+        this.Type = movie.Type
+        this.Year = movie.Year 
     }
 
     render() {
@@ -44,98 +85,53 @@ class Movie {
         let h4 = document.createElement("h4")
         let h5 = document.createElement("h5")
         let btn = document.createElement("button")
+        let div = document.createElement("div")
 
         // Ajouter du contenu à ces éléments 
-        h3.textContent = this.title
-        img.src = this.poster
-        h4.textContent = this.year
-        h5.textContent = this.type
+        h3.textContent = this.Title
+        img.src = this.Poster
+        h4.textContent = this.Year
+        h5.textContent = this.Type
         btn.textContent = "Ajouter aux favoris"
 
         btn.addEventListener("click", () => {
-            // Ajout du movie dans les favs
-            favs.push(this)
-            // On enregistre aussi en LS
-            localStorage.setItem("favorites", JSON.stringify(favs))
+            let favs = new FavManager()
+            favs.addToFav(this)
         })
 
         // Insérer ces éléments dans notre div de container 
-        resultsZone.append(h3, img, h4, h5, btn)
-    }
+        div.append(h3, img, h4, h5, btn)
 
-}
-
-// Fonction de recherche de films 
-async function searchMovie(query) {
-    resultsZone.innerHTML = ""
-    input.value = ""
-
-    const res = await fetch(apiUrl + query, { headers : {
-        "Accept": "application/json"
-    }})
-
-    const data = await res.json()
-
-    if (!data.Response) {
-        resultsZone.innerHTML = "Aucun film trouvé ... " + data.Error
-        return
-    } else {
-
-        let movies = data.Search 
-        console.log(movies)
-
-        movies.forEach(movie => {
-            // Méthode de type fonctionnelle : :
-            // displayMovie(movie)
-
-            // Méthode de type POO
-            new Movie(movie).render()
-        });
+        // Le render nous retourne donc une div avec toutes les infos attendues
+        return div
     }
 }
 
+// Cette classe permet de gérer les aspect liés aux favoris 
+class FavManager {
+    // Ici on recup les favs soit du LS soit oninitialise en tableau vide (dans un constructor par exemple)
+    // Ici on gère l'ajout aux favoris 
+    // Pkoi pas une méthode de type gfetter qui liste l'ensemble des favoris (on pourra la réutiliser 
+    // dans la classe MovieAPp dans une méthode de type showFav par exemple)
 
-// Ecouteurs d'événements 
-searchBtn.addEventListener("click", () => {
-    searchMovie(input.value)
-})
-
-favBtn.addEventListener("click", () => {
-    resultsZone.innerHTML = ""
-
-    if (!favs.length) {
-        resultsZone.innerText = "Aucun favori enregistré"
-    } else {
-        favs.forEach(fav => {
-            new Movie(fav).render()
-        })
+    constructor() {
+        this.favs = JSON.parse(localStorage.getItem("favorites")) || []
     }
-}) 
+
+    getFavs() {
+        return this.favs
+    }
+
+    addToFav(movie) {
+        // Ajout du movie dans les favs
+        this.favs.push(movie)
+
+        // On enregistre aussi en LS
+        localStorage.setItem("favorites", JSON.stringify(this.favs))
+    }
+}
+
+// On instancie MovieApp afin de créer/initialiser l'application
+new MovieApp()
 
 
-
-// function displayMovie(movie) {
-//     // Créer les éléments HTML : div, titre, image etc 
-//     let h3 = document.createElement("h3")
-//     let img = document.createElement("img")
-//     let h4 = document.createElement("h4")
-//     let h5 = document.createElement("h5")
-//     let btn = document.createElement("button")
-
-//     // Ajouter du contenu à ces éléments 
-//     h3.textContent = movie.Title
-//     img.src = movie.Poster
-//     h4.textContent = movie.Year
-//     h5.textContent = movie.Type
-//     btn.textContent = "Ajouter aux favoris"
-
-//     btn.addEventListener("click", () => {
-//         // Ajout du movie dans les favs
-//         favs.push(movie)
-//         // On enregistre aussi en LS
-//         localStorage.setItem("favorites", JSON.stringify(favs))
-//     })
-
-//     // Insérere ces éléments dans notre div de container 
-//     resultsZone.append(h3, img, h4, h5, btn)
-// }
---
+++ philo.js
@@ -0,0 +1,12 @@
+// PHILO APP 
+
+// On va utiliser la philo API -> https://philosophyapi.pythonanywhere.com/api/
+
+// Objectif : 
+
+// 1 - Afficher une citation random sur notre page -> La citatiobn entre guillemets + L'auteur
+// 2 - A l'aide df'un bouton de refresh on pourra gébnérer à nouveau une citation lorsque l'on clique dessus 
+// 3 - En plus du bouton de refresh je veux un autre bouton "Wiki" -> quand je clique sur cet autre bouton 
+//     Je veux afficher dans un petit container le résumé wikipedia de l'auteur de la citation 
+
+// Vous irez chercher par vous meme le endpoint pour Wikipedia :)
